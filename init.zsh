1564e466d544d0bc2fbd745b7d2f94a6aec61e5d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-18 19:24:00 -0400

    more

--- init.zsh
+++ init.zsh
@@ -1,5 +1,22 @@
 p6df::modules::zsh::version() { echo "0.0.1" }
-p6df::modules::zsh::deps()    { ModuleDeps=(p6m7g8/p6common) }
+p6df::modules::zsh::deps()    { 
+	ModuleDeps=(
+		zsh-users/zsh-completions
+		zsh-users/zsh-syntax-highlighting
+		zsh-users/zsh-history-substring-search
+		zsh-users/zsh-autosuggestions
+
+		hlissner/zsh-autopair
+		zdharma/history-search-multi-word
+		zdharma/fast-syntax-highlighting
+
+		sorin-ionescu/prezto:modules/history
+
+#		jimeh/zsh-peco-history
+#		psprint/zsh-navigation-tools
+#		psprint/zsh-editing-workbench
+	)
+}
 
 p6df::modules::zsh::external() { }
 
