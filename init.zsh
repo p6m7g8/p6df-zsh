60eae1536756396df467a21241e4d47a4b3bcc2c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-19 13:13:43 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 p6df::modules::zsh::version() { echo "0.0.1" }
-p6df::modules::zsh::deps()    { 
+p6df::modules::zsh::deps()    {
 	ModuleDeps=(
 		zsh-users/zsh-completions
 #		zsh-users/zsh-syntax-highlighting
@@ -12,6 +12,7 @@ p6df::modules::zsh::deps()    {
 
 		sorin-ionescu/prezto:modules/history
 
+		robbyrussell/oh-my-zsh:lib/diagnostics
 #		jimeh/zsh-peco-history
 #		psprint/zsh-navigation-tools
 #		psprint/zsh-editing-workbench
@@ -31,11 +32,25 @@ p6df::modules::zsh::external::brew() {
 p6df::modules::zsh::init() {
 
   p6df::modules::zsh::colors::init
+  p6df::modules::zsh::vcs::init
+  p6df::modules::zsh::comp::init
 }
 
 p6df::modules::zsh::colors::init() {
 
   autoload -U colors && colors
+}
+
+p6df::modules::zsh::vcs::init() {
+
+  autoload -Uz vcs_info
+}
+
+p6df::modules::zsh::comp::init() {
+
+  autoload -U compaudit compinit
+  compaudit
+  compinit -i -d ${ZDOTDIR}/.zcompdump
 }
 
 p6df::modules::zsh::init
